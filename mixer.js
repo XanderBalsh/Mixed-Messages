bfd1eb3e94863c10e4b98c86a1a071f2fd41c9df
XanderBalsh <170201275+XanderBalsh@users.noreply.github.com>
2024-09-08 14:34:10 +0100

    Initialised basic functionality for fortune.

---
+++ mixer.js
@@ -0,0 +1,18 @@
+// Below I will store message components.
+const messages = ["You shall find ", "You shall not find ", "What you seek is "];
+const messages2 = ["Mr Monkey is not what you expect.", "the answers at the bottom of a bottle.", "more questions than answers."];
+
+// Below is a function that randomly picks one of each messages and combines them.
+
+const messageMaker = () => {
+    let opt1 = messages[Math.floor(Math.random() * 3)];
+    console.log(opt1);
+    let opt2 = messages2[Math.floor(Math.random() * 3)];
+    console.log(opt2);
+    let prophecy = opt1 + opt2;
+    
+    return prophecy;
+};
+
+console.log(messageMaker());
+
